3148f1cfc3accbec0f5ab80ff26a58886a5477f2
Funkymonkey28 <67377970+N-Quan@users.noreply.github.com>
2022-12-22 14:26:58 +1100

    added server setup files

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo apt update 
+sudo apt full-upgrade
+
+#Docker
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker pi
+
+#FanControl
+curl https://download.argon40.com/argonfanhat.sh | bash
+
+#NordVPN
+sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
+
+#Allow devices on same network to access server
+nordvpn whitelist remove all
+nordvpn whitelist add subnet 192.168.0.1/24 # Router IP is 192.168.0.1
+nordvpn connect
+
+### Media Server - setup done with docker compose file ###
+#QBittorrent
+#Jellyfin
+#Prowlarr
+#Radarr 
+#Sonarr
